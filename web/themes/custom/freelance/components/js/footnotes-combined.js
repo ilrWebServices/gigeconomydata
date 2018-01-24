b9ae3c3e6469afa401785c432aab29ba458238e4
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-01-24 16:02:56 -0500

    Fixing toc bug and updating toc styles

--- web/themes/custom/freelance/components/js/footnotes-combined.js
+++ web/themes/custom/freelance/components/js/footnotes-combined.js
@@ -3,12 +3,12 @@
     attach: function (context, settings) {
       var footnotes_html = '';
       var $i = 1;
-      $('ul.footnotes').not('.footnotes--combined').each(function () {
+      $('li.footnote').each(function () {
         var $link = $(this).find('a');
         var $href = $link.attr('href');
         $link.text($i+'.'); // Update the footnote li
         $('a'+$href).text($i); // Update the link text
-        footnotes_html += $(this).html();
+        footnotes_html += $(this)[0].outerHTML;
         $(this).remove();
         $i++;
       });
@@ -17,6 +17,8 @@
         $('#footnotes-combined').prepend(
           '<ul class="footnotes footnotes--combined">'
           + footnotes_html + '</ul>');
+        // Clean up the old footnotes lists
+        $('ul.footnotes').not('.footnotes--combined').remove();
       }
     }
   };
