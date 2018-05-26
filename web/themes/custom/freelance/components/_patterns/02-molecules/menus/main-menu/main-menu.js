a85d6593e60a80f6c047424d697c961261677105
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-05-26 10:27:55 -0400

    Beginning implementation of hover menu

--- web/themes/custom/freelance/components/_patterns/02-molecules/menus/main-menu/main-menu.js
+++ web/themes/custom/freelance/components/_patterns/02-molecules/menus/main-menu/main-menu.js
@@ -10,37 +10,33 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 
 
-// (function (Drupal) { // UNCOMMENT IF DRUPAL.
-//
-//   Drupal.behaviors.mainMenu = {
-//     attach: function (context) {
+(function (Drupal) { // UNCOMMENT IF DRUPAL.
 
-(function () { // REMOVE IF DRUPAL.
+  Drupal.behaviors.mainMenu = {
+    attach: function (context) {
+      'use strict';
 
-  'use strict';
+      // Use context instead of document IF DRUPAL.
+      var toggle_expand = document.getElementById('toggle-expand');
+      var menu = document.getElementById('main-nav');
+      var expand_menu = menu.getElementsByClassName('expand-sub');
 
-  // Use context instead of document IF DRUPAL.
-  var toggle_expand = document.getElementById('toggle-expand');
-  var menu = document.getElementById('main-nav');
-  var expand_menu = menu.getElementsByClassName('expand-sub');
+      // Mobile Menu Show/Hide.
+      toggle_expand.addEventListener('click', function (e) {
+        toggle_expand.classList.toggle('toggle-expand--open');
+        menu.classList.toggle('main-nav--open');
+      });
 
-  // Mobile Menu Show/Hide.
-  toggle_expand.addEventListener('click', function (e) {
-    toggle_expand.classList.toggle('toggle-expand--open');
-    menu.classList.toggle('main-nav--open');
-  });
+      // Expose mobile sub menu on click.
+      for (var i = 0; i < expand_menu.length; i++) {
+        expand_menu[i].addEventListener('click', function (e) {
+          var menu_item = e.currentTarget;
+          var sub_menu = menu_item.nextElementSibling;
 
-  // Expose mobile sub menu on click.
-  for (var i = 0; i < expand_menu.length; i++) {
-    expand_menu[i].addEventListener('click', function (e) {
-      var menu_item = e.currentTarget;
-      var sub_menu = menu_item.nextElementSibling;
-
-      menu_item.classList.toggle('expand-sub--open');
-      sub_menu.classList.toggle('main-menu--sub-open');
-    });
-  }
-
-})(); // REMOVE IF DRUPAL.
-
-// })(Drupal); // UNCOMMENT IF DRUPAL.
+          menu_item.classList.toggle('expand-sub--open');
+          sub_menu.classList.toggle('main-menu--sub-open');
+        });
+      }
+    }
+  };
+}(Drupal)); // UNCOMMENT IF DRUPAL.
