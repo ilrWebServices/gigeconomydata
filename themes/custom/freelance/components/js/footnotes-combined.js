3d94efdc5eb8acc28dbea01c890fca82cf6466ef
Aaron Froehlich <aaronfroehlich@gmail.com>
2025-06-20 13:28:21 -0400

    Update site with new logos and final content
    This commit also removes the aggregated CSS and JS files, in case they need to be updated again manually.

---
+++ themes/custom/freelance/components/js/footnotes-combined.js
@@ -0,0 +1,25 @@
+(function ($) {
+  Drupal.behaviors.footnotesCombined = {
+    attach: function (context, settings) {
+      var footnotes_html = '';
+      var $i = 1;
+      $('li.footnote').each(function () {
+        var $link = $(this).find('a.footnote-label');
+        var $href = $link.attr('href');
+        $link.text($i+'.'); // Update the footnote li
+        $('a'+$href).text($i); // Update the link text
+        footnotes_html += $(this)[0].outerHTML;
+        $(this).remove();
+        $i++;
+      });
+
+      if (footnotes_html) { // Make sure the footnotes are not empty
+        $('#footnotes-combined').prepend(
+          '<ul class="footnotes footnotes--combined">'
+          + footnotes_html + '</ul>');
+        // Clean up the old footnotes lists
+        $('ul.footnotes').not('.footnotes--combined').remove();
+      }
+    }
+  };
+})(jQuery);
